3a3be502e2d24e9d70cf3b688ecfc1402f3eca03
amanketebo <amanuelketebo@yahoo.com>
2017-12-19 18:12:43 -0800

    Add colors to pokemon types

--- Pokédex/Model/TypesView.swift
+++ Pokédex/Model/TypesView.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 
+enum Type: String {
+    case grass = "Grass"
+    case poison = "Poison"
+    case fire = "Fire"
+    case flying = "Flying"
+    case water = "Water"
+    case electric = "Electric"
+    case ground = "Ground"
+    case fairy = "Fairy"
+    case bug = "Bug"
+    case fighting = "Fighting"
+    case psychic = "Psychic"
+    case rock = "Rock"
+    case steel = "Steel"
+    case normal = "Normal"
+}
+
 class TypesView: UIView {
     var types: [String] = [] {
         didSet {
@@ -20,8 +37,16 @@ class TypesView: UIView {
     var typeLabelWidthPadding: CGFloat = 8
     var typeLabelHeightPadding: CGFloat = 8
     var typeLabelCornerRadius: CGFloat = 5
+    var typeLabelLeftSpacing: CGFloat = 3
     
     private var currentOffset: CGFloat = 0
+    private var typeColors: [Type: UIColor] = [.grass: Theme.Colors.grassGreen, .poison: Theme.Colors.poisonBlue,
+                                               .fire: Theme.Colors.fireRed, .flying: Theme.Colors.flyingBlue,
+                                               .water: Theme.Colors.waterBlue, .electric: Theme.Colors.electricGray,
+                                               .ground: Theme.Colors.groundBlack, .fairy: Theme.Colors.fairyPurple,
+                                               .bug: Theme.Colors.bugGreen, .fighting: Theme.Colors.fightingPink,
+                                               .psychic: Theme.Colors.psychicYellow, .rock: Theme.Colors.rockLightGray,
+                                               .steel: Theme.Colors.steelBrown]
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -33,9 +58,17 @@ class TypesView: UIView {
             label.text = type
             label.font = UIFont(name: "Menlo-Bold", size: 12)
             label.textColor = .white
-            label.backgroundColor = .red
             label.textAlignment = .center
             label.cornerRadius = typeLabelCornerRadius
+            
+            if let type = Type(rawValue: type),
+                let typeColor = typeColors[type] {
+                label.backgroundColor = typeColor
+            } else {
+                label.borderColor = .black
+                label.textColor = .black
+                label.borderWidth = 1
+            }
             
             var size = label.intrinsicContentSize
             size.width += typeLabelWidthPadding
@@ -49,6 +82,7 @@ class TypesView: UIView {
         typeLabels.forEach { (typeLabel) in
             typeLabel.frame.origin.x += currentOffset
             currentOffset += typeLabel.bounds.width
+            currentOffset += typeLabelLeftSpacing
             addSubview(typeLabel)
         }
     }
